319af5f2444bc56032f28eefb9ee803d2505ce6b
Lucas Moraes <lucasmoraes@Lucass-MacBook-Pro.local>
2022-04-10 20:37:05 -0400

    express

---
+++ client/build/precache-manifest.bc9b6aecf1b83332d705371a7c368748.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdadcfa9fdbbc4eff351088757aad16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a7f5d51da186a18452",
+    "url": "/static/css/main.4f009154.chunk.css"
+  },
+  {
+    "revision": "c907713a909473ce466a",
+    "url": "/static/js/2.27e57e9d.chunk.js"
+  },
+  {
+    "revision": "91a7f5d51da186a18452",
+    "url": "/static/js/main.43ea0c01.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
