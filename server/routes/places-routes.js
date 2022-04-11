6e5dae97886b6698f249ddc926690dce4c2b92ff
Lucas Moraes <lucasmoraes@Lucass-MacBook-Pro.local>
2022-04-11 10:28:35 -0400

    places routes started, no res

---
+++ server/routes/places-routes.js
@@ -0,0 +1,10 @@
+var  express = require('express');
+
+const router = express.Router();
+
+router.get ('/', (req, res, next) => {
+    console.log('GET Request in places');
+    res.json({message: 'It works!'});
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-var  express = require('express'),
-         app = express();
+var  express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
 const path = require("path");
-    
+   
+const placesRoutes = require('./routes/places-routes');
+
 app.use(express.static(__dirname + "/../client/build"));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/../client/build", "index.html"));
    });
+app.use(placesRoutes);
 
 app.listen(
     process.env.PORT || 3030)
