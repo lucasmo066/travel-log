1c1b2e01c74a6c4cda6a0f4798e9106ab49d8f25
Lucas Moraes <lucasmoraes@Lucass-MacBook-Pro.local>
2022-04-12 17:33:14 -0400

    database complete

--- server/server.js
+++ server/server.js
@@ -2,14 +2,29 @@ var  express = require('express')
 const bodyParser = require('body-parser');
 const app = express();
 const path = require("path");
-   
+const mongoose = require('mongoose');
+
+const usersRoutes = require('./routes/users-routes');
 const placesRoutes = require('./routes/places-routes');
 
+app.use(bodyParser.json());
+
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
+
+// all get routes above catch all
 app.use(express.static(__dirname + "/../client/build"));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/../client/build", "index.html"));
    });
-app.use(placesRoutes);
 
-app.listen(
-    process.env.PORT || 3030)
+
+   mongoose
+   .connect(process.env.MONGODB_URI)
+   .then(() => {
+     app.listen(process.env.PORT || 3000);
+   })
+   .catch(err => {
+     console.log(err);
+   });
+ 
